401571386fc55b3207029d8537b15b73370eab17
omcbride <omcbride@isitedesign.com>
2013-09-10 16:07:30 -0400

    added ipmanager

---
+++ Tester/IpManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tester
+{
+    public class IpManager
+    {
+        public static string GetIp(string ips)
+        {
+            var ip = string.Empty;
+
+            var ipArray = ips.Split(',');
+            ip = ipArray.GetValue(0).ToString().Trim();
+            return ip;
+        }
+    }
+}
