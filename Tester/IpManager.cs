57797094b1f1621a40f4cc55858c1600031bcb89
omcbride <omcbride@isitedesign.com>
2013-09-10 16:34:35 -0400

    added removeip

--- Tester/IpManager.cs
+++ Tester/IpManager.cs
@@ -16,5 +16,17 @@ namespace Tester
             ip = ipArray.GetValue(0).ToString().Trim();
             return ip;
         }
+
+        public static string RemoveIp(string ips)
+        {
+            var ip = string.Empty;
+
+            var ipArray = ips.Split(',');
+            for (var i = 0; i < ips.Count() - 1; i++)
+            {
+                ip += i;
+            }
+            return ip;
+        }
     }
 }
